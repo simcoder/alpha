a223a1c82754f902b84da6bd1a18d6be6cb21e74
Robinson Aizprua <robinsonaizprua@simcodertutorials.com>
2019-05-01 23:16:13 -0500

    add docker

--- ROAM/src/environments/environment.prod.ts
+++ ROAM/src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
   auth:{
-    clientID: "",
-    domain: "",
-    callbackURL: ""
+    clientID: "NcXmlrI17X6go0HBA0z2vHAos8KD7zhg",
+    domain: "simcoder.auth0.com",
+    callbackURL: "http://localhost:8080/callback",
+    audience: 'YOUR-AUTH0-API-IDENTIFIER',
+    redirect: 'http://localhost:8080/callback',
+    scope: 'openid profile email'
   }
 };
