ca7e300cb4727f80e422d937d0800ada59b6e94f
Robinson <robinsonaizprua@simcodertutorials.com>
2019-07-06 00:20:30 -0500

    payment support

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+admin.initializeApp(functions.config().firebase);
+
+const stripe = require('stripe')(functions.config().stripe.testkey)
+
+
+
+exports.stripeCharge = functions.database
+                                .ref('/payments/{userId}/{paymentId}')
+                                .onCreate((event, context) => {
+                          
+  const payment = event._data;
+  const userId = context.params.userId;
+  const paymentId = context.params.paymentId;
+  
+  // checks if payment exists or if it has already been charged
+  if (!payment || payment.charge) return;
+
+  return admin.database()
+              .ref(`/users/${userId}`)
+              .once('value')
+              .then(snapshot => {
+                  console.log(snapshot);
+                  return snapshot.val();
+               })
+               .then(customer => {
+                 console.log(customer);
+                 const amount = payment.amount;
+                 const idempotency_key = paymentId;  // prevent duplicate charges
+                 const source = payment.token.id;
+                 const receipt_email = 'robinson@mydicoin.com';
+                 const currency = 'usd';
+                 const charge = {amount, currency, source, receipt_email};
+                 return stripe.charges.create(charge, { idempotency_key });
+               }).then(charge => {
+                   admin.database()
+                        .ref(`/payments/${userId}/${paymentId}/charge`)
+                        .set(charge)
+                   return;
+                  })
+});
